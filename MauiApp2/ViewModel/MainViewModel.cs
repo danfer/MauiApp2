59c082a1d5fcedfffdff1f8a3f5bee0a7af645a6
Danfer Habed <dhabed@outlook.com>
2022-09-13 08:26:25 -0600

    Adding observables

---
+++ MauiApp2/ViewModel/MainViewModel.cs
@@ -0,0 +1,12 @@
+﻿
+using CommunityToolkit.Mvvm.ComponentModel;
+using System.ComponentModel;
+
+namespace MauiApp2.ViewModel
+{
+    public partial class MainViewModel : ObservableObject
+    {
+        [ObservableProperty]
+        string text;       
+    }
+}
